ba2ba51abe08e6dc7abdeec4e653314875a6c1fc
alexdzius <akeldwithaviation@gmail.com>
2021-01-27 14:45:35 -0800

    updated stuff

---
+++ BackToTheHiggins/Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * EnemyCollison.cs
+ * By: Alex Dzius
+ * Last Edited: 1/27/2021
+ * Script to reload scene upon death (colliding with an 'enemy object'
+ */ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EnemyCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
