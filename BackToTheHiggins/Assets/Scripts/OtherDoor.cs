a4a5d02c4844876c8b07c34f5bce8b31d23fedcd
bluephaser <liam.binford@digipen.edu>
2021-02-01 12:58:04 -0800

    qwerty

---
+++ BackToTheHiggins/Assets/Scripts/OtherDoor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OtherDoor : MonoBehaviour
+{
+    public string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if (gameObject.tag == "LastDoor" && GameManager.Divergence != 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+}
+
+
